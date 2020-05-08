1144f54af3259e1542c5cafd73366fddde68c992
Roman Amici <ramici@pop-os.localdomain>
2020-05-08 11:49:33 -0600

    Added percent noise to util

--- util.py
+++ util.py
@@ -2,6 +2,11 @@ from scipy.io import loadmat
 import numpy as np
 
 
+def percent_noise(U, noise_percent):
+    std = np.std(U[:, 0])*noise_percent
+    return U + np.random.normal(0, std, size=U.shape)
+
+
 def load_mat_2d(filename, x1_key, x2_key, u_key):
     data = loadmat(filename)
 
@@ -26,6 +31,30 @@ def ungrid_u_2d(x1, x2, u):
             U[idx, 0] = u[i, j]
 
     return X, U
+
+
+def get_u_const_idx_2d(x1, x2, u, idx_1=None, idx_2=None):
+
+    if idx_1 is not None:
+        U = np.zeros((x2.shape[0], 1))
+        X = np.zeros((x2.shape[0], 2))
+        i = idx_1
+        for j in range(x2.shape[0]):
+            U[j, 0] = u[i, j]
+            X[j, 0], X[j, 1] = x1[i], x2[j]
+
+        return X, U
+    elif idx_2 is not None:
+        U = np.zeros((x1.shape[0], 1))
+        X = np.zeros((x1.shape[0], 2))
+        j = idx_2
+        for i in range(x1.shape[0]):
+            U[i, 0] = u[i, j]
+            X[i, 0], X[i, 1] = x1[i], x2[j]
+
+        return X, U
+    else:
+        return None
 
 
 def to_grid_u_2d(nx1, nx2, U):
